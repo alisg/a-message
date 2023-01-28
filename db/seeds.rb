0dd5e8ed2f62e84750c053b524d00cfbc53cb21b
alisg <alisagrigoryan1@gmail.com>
2023-01-28 18:15:42 +0400

    Adding User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "lil", password: "password")
+User.create(username: "lik", password: "password")
+User.create(username: "lek", password: "password")
+User.create(username: "lesh", password: "password")
+User.create(username: "lin", password: "password")
